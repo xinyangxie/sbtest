87b813ba66bb49ff20c04f86b811f7ecfa02fb43
xiexinyang <17739216203@163.com>
2018-12-06 09:50:11 +0800

    testone

---
+++ src/main/java/com/xxy/sbtest/SbtestApplication.java
@@ -0,0 +1,12 @@
+package com.xxy.sbtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbtestApplication.class, args);
+    }
+}
