a997400ced4ce41d0c4a21c6ab2821469ebec69a
xiexinyang <17739216203@163.com>
2018-12-10 18:23:47 +0800

    testtwo

--- src/test/java/com/xxy/sbtest/Test1.java
+++ src/test/java/com/xxy/sbtest/Test1.java
@@ -2,12 +2,18 @@ package com.xxy.sbtest;
 
 import org.junit.Test;
 
+import java.util.Date;
+
 public class Test1 {
 
 @Test
     public void testone(){
-
-    int length = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9".length();
-    System.out.println(length);
+    long b=24*60*60*1000L;
+    Date date=new Date();
+    System.out.println(date);
+    System.out.println(date.getTime());
+    long l = date.getTime() - b;
+    Date date1=new Date(l);
+    System.out.println(date1);
 }
 }
